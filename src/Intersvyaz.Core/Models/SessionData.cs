05642717c8cfd11bd906b963d01ec53be7b47f0e
synzr <mikhail@synzr.space>
2025-11-19 11:01:03 +0500

    Описан интерфейс сервиса сессии пользователя.

---
+++ src/Intersvyaz.Core/Models/SessionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intersvyaz.Core.Models
+{
+    public class SessionData
+    {
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+    }
+}
---
+++ src/Intersvyaz.Core/Services/ISessionService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Intersvyaz.Core.Models;
+
+namespace Intersvyaz.Core.Services
+{
+    public interface ISessionService
+    {
+        /// <summary>
+        /// Получить сессию пользователя. При отсутствие сессии,
+        /// входит в систему автоматически.
+        /// </summary>
+        /// <param name="username">Имя пользователя.</param>
+        /// <param name="password">Пароль пользователя.</param>
+        /// <returns>Данные об сессии.</returns>
+        Task<SessionData> GetSessionAsync(string username, string password);
+
+        /// <summary>
+        /// Получить сессию пользователя.
+        /// </summary>
+        /// <returns>Данные об сессии.</returns>
+        SessionData GetSession();
+
+        /// <summary>
+        /// Проверить, есть ли сессия пользователя.
+        /// </summary>
+        bool HasSession();
+
+        /// <summary>
+        /// Удалить сессию пользователя.
+        /// </summary>
+        void DeleteSession();
+    }
+}
