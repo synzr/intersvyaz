01f9c3aa2fb6826d924f043ec251e2eb95c36f72
synzr <mikhail@synzr.space>
2025-11-21 10:41:52 +0500

    Реализован сервис настроек приложения.

---
+++ src/Intersvyaz.Core/Services/ISettingsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Intersvyaz.Core.Services
+{
+    public interface ISettingsService
+    {
+        /// <summary>
+        /// Событие об изменении текущего пользователя.
+        /// </summary>
+        event EventHandler<string> CurrentUserChanged;
+
+        /// <summary>
+        /// Получить текущего пользователя.
+        /// </summary>
+        /// <returns>Имя пользователя.</returns>
+        string GetCurrentUser();
+
+        /// <summary>
+        /// Установить нового текущего пользователя.
+        /// </summary>
+        /// <param name="value">Имя нового пользователя.</param>
+        void SetCurrentUser(string value);
+    }
+}
---
+++ src/Intersvyaz.Core/Services/SettingsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.Storage;
+
+namespace Intersvyaz.Core.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        /// <inheritdoc />
+        public event EventHandler<string> CurrentUserChanged;
+
+        /// <inheritdoc />
+        public string GetCurrentUser()
+        {
+            return (string)ApplicationData.Current.LocalSettings.Values["CurrentUser"] ?? null;
+        }
+
+        /// <inheritdoc />
+        public void SetCurrentUser(string value)
+        {
+            ApplicationData.Current.LocalSettings.Values["CurrentUser"] = value;
+            CurrentUserChanged?.Invoke(this, value);
+        }
+    }
+}
